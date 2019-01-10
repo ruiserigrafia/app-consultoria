f272682ef21e7164f8e09e91336467bc556efb10
ruiserigrafia <ruiserigrafia@gmail.com>
2019-01-09 23:26:25 -0300

    criando o pacote app

---
+++ src/app/Main.java
@@ -0,0 +1,73 @@
+package app;
+
+import ajudantes.NotificadorAlertas;
+import controller.FormLoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private static Main instancia = new Main();
+    private static Stage palco;
+    private AnchorPane root;
+    private Scene cena;
+    private FXMLLoader loader;
+    private FormLoginController controller;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            initComponents();
+            Main.palco = primaryStage;
+            initStage();
+        } catch(Exception e) {
+            NotificadorAlertas.mostrarMsgErro(e,"Não foi possível inicializar o sistema. Por favor tente novamente.\r\n" +
+                    "Se o erro persistir contate o desenvolvedor do sistema");
+        }
+    }
+
+    public static Stage getStage() {
+        return palco;
+    }
+
+    protected void initComponents() throws IOException {
+        loader = new FXMLLoader();
+        root = new AnchorPane();
+
+        loader.setLocation(getClass().getResource("/view/formLogin.fxml"));
+        root = (AnchorPane)loader.load();
+        this.loader.setController(controller);
+        cena = new Scene(root);
+        cena.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+    }
+
+    protected void initStage() {
+        palco = new Stage();
+        palco.setScene(cena);
+        palco.centerOnScreen();
+        palco.setResizable(false);
+        palco.initStyle(StageStyle.UTILITY);
+        palco.setTitle("Sistema de Avaliação Física e Nutricional");
+        palco.show();
+    }
+
+    public static Main getInstancia() {
+        if (instancia == null)
+            instancia = new Main();
+        return instancia;
+    }
+
+    public static void main(String[] args)
+    {
+        getInstancia();
+        launch(args);
+
+    }
+
+}
